707cf550a239d3418168dc90c7bd3c55cd7db579
lancelotzomax <43225528+lancelotzomax@users.noreply.github.com>
2024-06-18 17:00:04 +0800

    step: final update

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from typing import Union
 from fastapi import FastAPI, File, UploadFile, HTTPException
-from fastapi.responses import FileResponse, RedirectResponse
-
-import starlette.status as status
-from pydantic import BaseModel
-
+from fastapi.responses import FileResponse
 import logging # for more logging info
 import uvicorn
 import os
@@ -12,37 +8,31 @@ import os
 # Directory where files will be saved
 UPLOAD_DIRECTORY = "./img/"
 
-# Ensure the directory exists
-if not os.path.exists(UPLOAD_DIRECTORY):
-    os.makedirs(UPLOAD_DIRECTORY)
-
+# Create the FastAPI Instance 
 app = FastAPI(title="Fast API",
               description="API for Image Upload")
 
 LOG = logging.getLogger(__name__)
-LOG.info("API")
+LOG.info("API is starting up")
 LOG.info(uvicorn.Config.asgi_version)
 
 # router = APIRouter
 
-class Item(BaseModel):
-    name:str
-    item_id:int
-    
-
 @app.get("/")
 def read_root():
-    # redirect_url = request.url_for('signin')+ '?x-error=Invalid+credentials'
-    #     return RedirectResponse(redirect_url, status_code=status.HTTP_302_FOUND, headers={"x-error": "Invalid credentials"})
     return {"Hello": "World"}
 
 
 @app.post("/image/{item_id}")
 async def upload_image(item_id: int, item_name: str, file: UploadFile = File(...)):
     try:
+        # Ensure the directory exists
+        if not os.path.exists(UPLOAD_DIRECTORY):
+            os.makedirs(UPLOAD_DIRECTORY)
+
         # Read the file contents
         contents = await file.read()
-
+        
         # Construct a file path
         file_path = os.path.join(UPLOAD_DIRECTORY, f"{item_id}_{item_name}.jpg")
 
@@ -119,3 +109,5 @@ def delete_item(item_id: int):
     return {"message": f"Successfully deleted file of item_id {item_id}"}
 
 
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
